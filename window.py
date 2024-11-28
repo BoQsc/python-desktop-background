e82bdb60e643e5422e596c77f067c0e82f84ea49
Feldwor <BoQsc@users.noreply.github.com>
2024-11-28 23:59:23 +0200

    Update window.py

--- window.py
+++ window.py
@@ -2,9 +2,11 @@ import tkinter
 window = tkinter.Tk()
 window.title("Window Canvas")
 
-canvas = tkinter.Canvas(bg="gray")
+canvas = tkinter.Canvas(bg="gray", bd=0, highlightthickness=0)
 
 
 
 window.bind("<Configure>", lambda event: canvas.place_configure(width=event.width, height=event.height))
+window.bind("<F11>", lambda event: window.attributes("-fullscreen", not window.attributes("-fullscreen")))
+
 window.mainloop()
