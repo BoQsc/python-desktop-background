4122962e2b48f11db409a2abc932de8557b1605e
Feldwor <BoQsc@users.noreply.github.com>
2024-11-28 23:09:19 +0200

    update

--- window.py
+++ window.py
@@ -3,10 +3,8 @@ window = tkinter.Tk()
 window.title("Window Canvas")
 
 canvas = tkinter.Canvas(bg="gray")
-canvas.place(x=0, y=0)
+
 
 
 window.bind("<Configure>", lambda event: canvas.place_configure(width=event.width, height=event.height))
-
-
-tkinter.mainloop()
+window.mainloop()
---
+++ window_canvas_static_size.py
@@ -0,0 +1,11 @@
+import tkinter
+window = tkinter.Tk()
+window.title("Window Canvas")
+
+canvas = tkinter.Canvas(bg="gray")
+canvas.place(x=0, y=0)
+
+
+
+
+tkinter.mainloop()
