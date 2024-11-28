67ba6b2ad39203a128da47a96dfb4cc5c6a54e6f
Feldwor <BoQsc@users.noreply.github.com>
2024-11-29 00:30:00 +0200

    Update window.py

--- window.py
+++ window.py
@@ -9,6 +9,7 @@ canvas.create_image(0, 0, anchor="nw",image=canvas_background)
 
 
 window.bind("<Configure>", lambda event: canvas.place_configure(width=event.width, height=event.height))
+# Implement background image resize based on canvas size.
 window.bind("<F11>", lambda event: window.attributes("-fullscreen", not window.attributes("-fullscreen")))
 
 window.mainloop()
