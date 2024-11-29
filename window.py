5f7ac2ad2cd217ccbaac688bb99710053bd5065d
Feldwor <BoQsc@users.noreply.github.com>
2024-11-29 08:46:05 +0200

    update

--- window.py
+++ window.py
@@ -10,7 +10,6 @@ canvas.create_image(0, 0, anchor="nw",image=canvas_background)
 
 window.bind("<Configure>", lambda event: canvas.place_configure(width=event.width, height=event.height))
 # Implement background image resize based on canvas size.
-# Monitor the performance of tkinter canvas. (frames executed)
 window.bind("<F11>", lambda event: window.attributes("-fullscreen", not window.attributes("-fullscreen")))
 
 window.mainloop()
