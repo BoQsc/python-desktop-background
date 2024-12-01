a69283c64c6f170234b54536121f5ef884dee336
Feldwor <BoQsc@users.noreply.github.com>
2024-12-02 00:45:09 +0200

    update

--- Fundamentals/window_background_subsample.py
+++ Fundamentals/window_background_subsample.py
@@ -6,9 +6,7 @@ window.title("Window Canvas")
 canvas = tkinter.Canvas(bg="gray", highlightthickness=0)
 
 canvas_background = tkinter.PhotoImage(file="background.png")
-canvas_background = canvas_background.subsample(2,2)
-canvas_background = canvas_background.subsample(2,2)
-
+canvas_background = canvas_background.subsample(5,5)
 canvas.create_image(0, 0, anchor="nw", image=canvas_background)
 
 window.bind("<Configure>", lambda event: canvas.place_configure(width=event.width, height=event.height))
