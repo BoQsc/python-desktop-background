5a91bef244766ffa4750ef4b5f7c95b8c3d09907
Feldwor <BoQsc@users.noreply.github.com>
2024-12-03 01:13:26 +0200

    Update window.py

--- Fundamentals2/window.py
+++ Fundamentals2/window.py
@@ -12,13 +12,12 @@ from PIL import Image, ImageTk
 background_image = Image.open(background_file)
 background_image = ImageTk.PhotoImage(background_image)
 
-background_image_object = tkinter.PhotoImage(file=background_image)
     
 
 canvas = tkinter.Canvas(bg="gray", highlightthickness=0)
 
 
-canvas_background_widget = canvas.create_image(0, 0, anchor="nw", image=background_image_object)
+canvas_background_widget = canvas.create_image(0, 0, anchor="nw", image=background_image)
 
 def resize_image(event):
 
