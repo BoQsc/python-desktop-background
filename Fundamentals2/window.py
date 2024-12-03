11e3a0f4c41f053027913dc25e227a9f32285daa
Feldwor <BoQsc@users.noreply.github.com>
2024-12-03 13:21:26 +0200

    Update window.py

--- Fundamentals2/window.py
+++ Fundamentals2/window.py
@@ -12,7 +12,7 @@ try:
 except ImportError:
     print("PIL Module is not installed.")
     
-print("_______TK Loading canvas widget__________")
+print("________TK Loading canvas widget__________")
 canvas = tkinter.Canvas(bg="gray", highlightthickness=0)
 canvas_background_widget = canvas.create_image(0, 0, anchor="nw", image=background_PhotoImage)
 
