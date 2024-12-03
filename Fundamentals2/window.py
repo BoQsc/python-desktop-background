31287ff6fffad9ed2907028798867f7aca1ee5c8
Feldwor <BoQsc@users.noreply.github.com>
2024-12-03 21:51:04 +0200

    update

--- Fundamentals2/window.py
+++ Fundamentals2/window.py
@@ -16,13 +16,17 @@ background_photo = ImageTk.PhotoImage(image=background_image)
 canvas_background_widget = canvas.create_image(0, 0, anchor="nw", image=background_photo)
 
 print("________Canvas Taskbar__________")
-taskbar_height = 50  
-taskbar = canvas.create_rectangle(0, canvas.winfo_height() - taskbar_height, canvas.winfo_width(), canvas.winfo_height(), fill="black", outline="")
+taskbar_height = 40
 
-taskbar_image = Image.new("RGBA", (canvas.winfo_width(), taskbar_height), (0, 0, 255, 128))  # (R, G, B, A)
+#rectangle_taskbar = canvas.create_rectangle(
+#    0, canvas.winfo_height() - taskbar_height, canvas.winfo_width(), canvas.winfo_height(), 
+#    fill="black", outline=""
+#)
+
+taskbar_image = Image.new("RGBA", (1, taskbar_height), (0, 0, 255, 128))  # Initial size of 1px
+taskbar_image = Image.open("taskbar.png")
 taskbar_photo = ImageTk.PhotoImage(taskbar_image)
-
-#taskbar = canvas.create_image(0, canvas.winfo_height() - taskbar_height, anchor="nw", image=taskbar_photo)
+image_taskbar = canvas.create_image(0, 0, anchor="nw", image=taskbar_photo)
 
 print("________TK Window Events__________")
 def background_image_resize(event):
@@ -31,17 +35,21 @@ def background_image_resize(event):
     canvas.itemconfig(canvas_background_widget, image=canvas.resized_photo)
 
 def taskbar_image_resize(event):
-    taskbar_image_resized = taskbar_image.resize((event.width, taskbar_height), Image.Resampling.LANCZOS)
-    canvas.taskbar_photo = ImageTk.PhotoImage(taskbar_image_resized)
-    canvas.itemconfig(taskbar, image=canvas.taskbar_photo)
+    resized_image = taskbar_image.resize((event.width, event.height), Image.Resampling.LANCZOS)
+    canvas.tresized_photo = ImageTk.PhotoImage(resized_image)
+    canvas.itemconfig(image_taskbar, image=canvas.tresized_photo)
+
+
 
 def on_window_event(event):
     canvas.config(width=event.width, height=event.height)
     background_image_resize(event)
-    canvas.coords(taskbar, 0, event.height - taskbar_height, event.width, event.height)
-    #canvas.coords(taskbar, 0, event.height - taskbar_height)
-    #taskbar_image_resize(event)
-
+    
+    taskbar_image_resize(event)
+    canvas.coords(image_taskbar, 0, event.height - taskbar_height)  
+    try:  
+        canvas.coords(rectangle_taskbar, 0, event.height - taskbar_height, event.width, event.height)
+    except: pass
 window.bind("<Configure>", on_window_event)
 window.bind("<F11>", lambda event: window.attributes("-fullscreen", not window.attributes("-fullscreen")))
 
