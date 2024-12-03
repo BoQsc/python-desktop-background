feb4143e63f3a1cd2b6753b2775ff105d6bdc455
Feldwor <BoQsc@users.noreply.github.com>
2024-12-03 21:22:49 +0200

    Update window.py

--- Fundamentals2/window.py
+++ Fundamentals2/window.py
@@ -29,16 +29,18 @@ def background_image_resize(event):
     resized_image = background_image.resize((event.width, event.height), Image.Resampling.LANCZOS)
     canvas.resized_photo = ImageTk.PhotoImage(resized_image)
     canvas.itemconfig(canvas_background_widget, image=canvas.resized_photo)
-    
+
+def taskbar_image_resize(event):
+    taskbar_image_resized = taskbar_image.resize((event.width, taskbar_height), Image.Resampling.LANCZOS)
+    canvas.taskbar_photo = ImageTk.PhotoImage(taskbar_image_resized)
+    canvas.itemconfig(taskbar, image=canvas.taskbar_photo)
 
 def on_window_event(event):
     canvas.config(width=event.width, height=event.height)
     background_image_resize(event)
     canvas.coords(taskbar, 0, event.height - taskbar_height, event.width, event.height)
     #canvas.coords(taskbar, 0, event.height - taskbar_height)
-    #taskbar_image_resized = taskbar_image.resize((event.width, taskbar_height), Image.Resampling.LANCZOS)
-    #canvas.taskbar_photo = ImageTk.PhotoImage(taskbar_image_resized)
-    #canvas.itemconfig(taskbar, image=canvas.taskbar_photo)
+    taskbar_image_resize(event)
 
 window.bind("<Configure>", on_window_event)
 window.bind("<F11>", lambda event: window.attributes("-fullscreen", not window.attributes("-fullscreen")))
