13c134dc5476dafddab753900b5da68f5ba40154
Feldwor <BoQsc@users.noreply.github.com>
2024-12-11 17:44:10 +0200

    Update background.py

--- Fundamentals4/background.py
+++ Fundamentals4/background.py
@@ -1,10 +1,7 @@
-import tkinter as tk
-from PIL import Image, ImageTk
-import threading
-import time
+import tkinter
 
-window = tk.Tk()
-canvas = tk.Canvas(window, bg="gray", highlightthickness=0)
+window = tkinter.Tk()
+canvas = tkinter.Canvas(window, bg="gray", highlightthickness=0)
 canvas.pack(fill="both", expand=True)
 
 on_window_event_callbacks = []
@@ -13,6 +10,7 @@ def on_window_event(event):
         callback(event)
 window.bind("<Configure>", on_window_event)
 
+from PIL import Image, ImageTk
 background_file = "background.png"
 background_image = Image.open(background_file)
 background_photo = ImageTk.PhotoImage(image=background_image)
@@ -21,15 +19,14 @@ canvas_background_widget = canvas.create_image(0, 0, anchor="nw", image=backgrou
 def resize_background_image(event, _last=[None, None]):
     if (event.width, event.height) != tuple(_last):
         _last[:] = [event.width, event.height]
+        
+        resized_image = background_image.resize((event.width, event.height), Image.Resampling.NEAREST)
+        canvas.resized_photo = ImageTk.PhotoImage(resized_image)
+        canvas.itemconfig(canvas_background_widget, image=canvas.resized_photo)
 
-        def resize_with_delay():
-            
-            resized_image = background_image.resize((event.width, event.height), Image.Resampling.NEAREST)
-            new_photo = ImageTk.PhotoImage(resized_image)
-            canvas.itemconfig(canvas_background_widget, image=new_photo)
-            canvas.photo = new_photo  # Prevent garbage collection
 
-        threading.Thread(target=resize_with_delay).start()
+def testz(event):
+    print("teeest")
 
 on_window_event_callbacks.append(resize_background_image)
 
