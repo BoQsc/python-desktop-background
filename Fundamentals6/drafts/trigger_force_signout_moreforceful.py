53655aa76db56541dac35726af4dcd0f7b3f9ec2
Feldwor <BoQsc@users.noreply.github.com>
2024-12-18 16:10:04 +0200

    update

--- Fundamentals6/drafts/trigger_force_signout_moreforceful.py
+++ Fundamentals6/drafts/trigger_force_signout_moreforceful.py
@@ -3,7 +3,7 @@ import ctypes
 # Function to shutdown the system
 def shutdown():
     user32 = ctypes.windll.user32
-    user32.ExitWindowsEx(0, 0)  # 1 for shutdown, 0 for no flags
+    user32.ExitWindowsEx(4, 0)  # 1 for EWX_SHUTDOWN flag
 
 # Call the shutdown function
 shutdown()
